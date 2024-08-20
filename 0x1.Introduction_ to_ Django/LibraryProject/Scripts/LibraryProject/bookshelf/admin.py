e83fbf313e2f1ea78bd33d518ed20d0a7e0bd835
Tiisetso <Tiisetsoatmash@gmail.com>
2024-08-20 22:30:10 +0200

    admin.py chamges

--- 0x1.Introduction_ to_ Django/LibraryProject/Scripts/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/Scripts/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Book
+
 
 # Register your models here.
+from .models import Book
 
 class BookAdmin(admin. ModelAdmin):
    list_filter = ('title', 'author', 'publication_year')
