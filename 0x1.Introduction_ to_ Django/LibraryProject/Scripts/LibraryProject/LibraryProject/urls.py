3457eaa94fc27e9ab7f7e3e2f24c665fb07d7471
Tiisetso <Tiisetsoatmash@gmail.com>
2024-08-20 22:25:00 +0200

    Django Interface

--- 0x1.Introduction_ to_ Django/LibraryProject/Scripts/LibraryProject/LibraryProject/urls.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/Scripts/LibraryProject/LibraryProject/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the LibraryProject Home Page!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
