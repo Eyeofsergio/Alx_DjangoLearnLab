42704264589a75920c5a1aa3b3f83ffc0a6a5c14
Eyeofsergio <Tiisetsoatmash@gmail.com>
2024-08-23 22:21:45 +0200

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .models import Book
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
-
 from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
 from .models import Library
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators omport permission_required 
+from django.contrib.auth.decortors import user_passes_test
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+
 
 class LibraryDetailView(DetailView):
     model = Library
