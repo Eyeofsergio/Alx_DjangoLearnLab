4a21465eb62632a3c3593994bf4063f4edd9173e
Tiisetso <Tiisetsoatmash@gmail.com>
2024-08-23 21:09:08 +0200

    new folders week 10

--- 0x1.Introduction_ to_ Django/LibraryProject/Scripts/LibraryProject/bookshelf/models.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/Scripts/LibraryProject/bookshelf/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    models.CharField, "max_length", 200, "title" , "author"
+    models.CharField, "max_length", 200 , "title" , "author"
     author = models.CharField , 100,
     publication_year = models.IntegerField()
 
