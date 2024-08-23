4bba282318817a078e244e24ab4388f2d6938e7f
Eyeofsergio <Tiisetsoatmash@gmail.com>
2024-08-23 22:55:57 +0200

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,6 +5,7 @@ from .views import view.register
 from django.contrib.auth import views as auth_views
 from librarian_view import librarian_view
 from member_view import member_view
+from django.contrib.auth iport views as auth_views
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -15,11 +16,4 @@ urlpatterns = [
     path(admin/', views.admin_view, name='admin'),
 ]
 
-from .views import views.register
-from django.contrib.auth import views as auth_views
 
-urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('register/', views.register.as_view(), name='register'),
-]
