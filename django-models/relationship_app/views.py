875a3593acd17a3c7d7facefbf02953eefa0a4ef
Eyeofsergio <Tiisetsoatmash@gmail.com>
2024-08-23 22:43:34 +0200

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Book
-from django.views.generic import DetailView
 from django.views.generic.detail import DetailView
 from .models import Library
 from django.contrib.auth import login
@@ -25,6 +24,7 @@ def register(request):
 
 def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
+
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
