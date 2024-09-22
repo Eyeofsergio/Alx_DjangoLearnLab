e8c8a63f0975805e5b637d4a6ec66e77e2d23967
Tiisetso <Tiisetsoatmash@gmail.com>
2024-09-23 01:09:35 +0200

    added crud operations to accounts

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import RegisterView, CustomAuthToken
+from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
+    path('follow/<int:user_id>/', views.follow_user, name='follow-user'),
+    path('unfollow/<int:user_id>/', views.unfollow_user, name='unfollow-user'),
+    path('feed/', views.user_feed, name='user-feed'),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,36 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.views import ObtainAuthToken
 from .serializers import RegisterSerializer, UserSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
+from .models import Post
+from .serializers import PostSerializer
+
+@api_view(['POST'])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    if user_to_follow != request.user:
+        request.user.following.add(user_to_follow)
+        return Response({'status': 'followed'}, status=status.HTTP_200_OK)
+    return Response({'error': 'You cannot follow yourself'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    if user_to_unfollow != request.user:
+        request.user.following.remove(user_to_unfollow)
+        return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
+    return Response({'error': 'You cannot unfollow yourself'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def user_feed(request):
+    followed_users = request.user.following.all()
+    posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
 
 class RegisterView(generics.CreateAPIView):
     queryset = Token.objects.all()
