35ac981936c4f5d47a8477a489356ad8d6f2d350
Tiisetso <Tiisetsoatmash@gmail.com>
2024-09-23 00:30:13 +0200

    accounts model.py updated

--- social_media_api/accounts/model.py
+++ social_media_api/accounts/model.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
+    bio = models.TextField()
     profile_picture = models.ImageField(default='default.jpg', upload_to='profile_pics/')
     followers = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='following')
 
