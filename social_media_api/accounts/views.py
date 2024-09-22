4089ed54eadf13e4aa31e6b1dfa0a31c7c11e764
Tiisetso <Tiisetsoatmash@gmail.com>
2024-09-23 01:23:10 +0200

    account Crud Operations for urls and views updated

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
