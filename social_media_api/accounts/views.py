ae6f75b697c23ed1a28326dfff492f7c8fb055ff
Tiisetso <Tiisetsoatmash@gmail.com>
2024-09-23 01:34:48 +0200

    account views updated

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,12 +1,11 @@
-from rest_framework import generics, status
+from rest_framework import generics, status, viewsets
 from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.views import ObtainAuthToken
 from .serializers import RegisterSerializer, UserSerializer
-from rest_framework import status
-from rest_framework import generics, status
+from accounts.serializers import UserSerializer, RegistrationSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -17,6 +16,40 @@ from .serializers import PostSerializer
 from .serializers import CustomUserSerializer
 from rest_framework import permissions 
 
+class RegisterAPIViewset(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.save()
+        user = data['user']
+        token = data['token']
+        return Response({
+            'token': token,
+            'user': UserSerializer(user, context=self.get_serializer_context()).data
+        }, status=status.HTTP_201_CREATED)
+    
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        user = CustomUser.objects.get(id=token.user_id)
+        return Response({
+            'token': token.key,
+            'user': UserSerializer(user).data
+        })
+
+class ProfileModelView(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return CustomUser.objects.filter(id=self.request.user.id)
+
 @api_view(['POST'])
 def follow_user(request, user_id):
     user_to_follow = get_object_or_404(CustomUser, id=user_id)
@@ -24,14 +57,6 @@ def follow_user(request, user_id):
         request.user.following.add(user_to_follow)
         return Response({'status': 'followed'}, status=status.HTTP_200_OK)
     return Response({'error': 'You cannot follow yourself'}, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['POST'])
-def unfollow_user(request, user_id):
-    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
-    if user_to_unfollow != request.user:
-        request.user.following.remove(user_to_unfollow)
-        return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
-    return Response({'error': 'You cannot unfollow yourself'}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def user_feed(request):
