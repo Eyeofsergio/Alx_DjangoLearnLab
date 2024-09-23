e845f83d7a79042c5aad0f0e45a7da7de71ba9bd
Tiisetso <Tiisetsoatmash@gmail.com>
2024-09-23 15:51:24 +0200

    Blog updated

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'blog.apps.BlogConfig',
+    'crispy_bootstrap5',
+    'crispy forms',
+    'blog.models',
 ]
 
 MIDDLEWARE = [
