2e22ee933f4f136300b3abd9eb9292f33292027d
Tiisetso <Tiisetsoatmash@gmail.com>
2024-09-23 02:14:04 +0200

    post views added generics Crud operation

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -54,7 +54,7 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
         
         if created:
@@ -73,7 +73,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         try:
             like = Like.objects.get(user=request.user, post=post)
             like.delete()
