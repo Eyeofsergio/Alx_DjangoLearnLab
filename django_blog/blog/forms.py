c952e46b175b9d3736cc041db45cf3f30429186a
Tiisetso <Tiisetsoatmash@gmail.com>
2024-09-23 16:39:15 +0200

    django Blog settigns updated with Tag op

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Comment
+from taggit.forms import TagWidget
+from .models import Post
+# Comment Form 
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -11,3 +14,13 @@ class CommentForm(forms.ModelForm):
         if len(content) < 5:
             raise forms.ValidationError("Comment must be at least 5 characters long.")
         return content
+    
+# Post Form
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget()
+        }
